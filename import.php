bdaa996b9c33b908958307449db70b67dbe2aa88
olimot <olimot@gmail.com>
2025-01-24 02:58:51 +0900

    delete import.php

--- import.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$numLine = 49151;
-$file = new SplFileObject('sudoku17.txt');
-//this is zero based so need to subtract 1
-$file->seek(rand(0,$numLine-1));
-//now print the line
-echo $file->current();
